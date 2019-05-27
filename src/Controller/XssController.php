8ba52dffb191f1de26e7876dda2205aa1e142ff4
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-05-27 18:08:43 +0200

    Xss exploit

--- src/Controller/XssController.php
+++ src/Controller/XssController.php
@@ -2,13 +2,43 @@
 
 namespace SecurityExamples\Controller;
 
+use Db;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use PrestaShopDatabaseException;
 use Symfony\Component\HttpFoundation\Request;
 
 class XssController extends FrameworkBundleAdminController
 {
+    /**
+     * Don't forget to disable xss protection
+     *
+     * Header set X-XSS-Protection 0
+     * http://ps-develop.localhost/admin-dev/index.php/modules/security-examples/xss?date-from=2018-10-08&date-to=2019-05-28%3Cscript%3Ealert(true);%3C/script%3E
+     */
     public function indexAction(Request $request)
     {
-        return $this->render('@Modules/security_examples/templates/admin/xss.html.twig');
+        $orders = [];
+        $dateFrom = $request->query->get('date-from');
+        $dateTo = $request->query->get('date-to');
+        if (!empty($dateFrom) && !empty($dateTo)) {
+            try {
+                $orders = Db::getInstance()->executeS(
+                    'SELECT * FROM ' . _DB_PREFIX_ . 'orders WHERE ' .
+                    'date_add BETWEEN "' . pSQL($dateFrom) . '" AND "' .
+                    pSQL($dateTo) . '"'
+                );
+            } catch (PrestaShopDatabaseException $e) {
+                // Don't care, we are awesome
+            }
+        }
+
+        return $this->render(
+            '@Modules/security_examples/templates/admin/xss.html.twig',
+            [
+                'orders' => $orders,
+                'dateFrom' => $dateFrom,
+                'dateTo' => $dateTo,
+            ]
+        );
     }
 }

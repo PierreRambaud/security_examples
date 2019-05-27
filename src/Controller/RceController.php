28059abd6a1304c851e097e8550384fe3018fa21
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2019-05-27 17:30:38 +0200

    Rce exploit

--- src/Controller/RceController.php
+++ src/Controller/RceController.php
@@ -2,13 +2,37 @@
 
 namespace SecurityExamples\Controller;
 
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Request;
 
 class RceController extends FrameworkBundleAdminController
 {
+    // exploit http://ps-develop.localhost/admin-dev/index.php/modules/security-examples/rce/command?type=router;whoami
     public function indexAction(Request $request)
     {
-        return $this->render('@Modules/security_examples/templates/admin/rce.html.twig');
+        $output = null;
+        $type = $request->query->get('type');
+        if (!empty($type)) {
+            $process = new Process(
+                sprintf(
+                    '%s %s/../bin/console debug:%s',
+                    PHP_BINARY,
+                    $this->getParameter('kernel.root_dir'),
+                    $type
+                )
+            );
+            $process->run();
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+            $output = $process->getOutput();
+        }
+
+        return $this->render(
+            '@Modules/security_examples/templates/admin/rce.html.twig',
+            ['output' => $output]
+        );
     }
 }
